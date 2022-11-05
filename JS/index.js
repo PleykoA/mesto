3d6b7391d6e6b516ff996af0a0f406a74962a773
Asya <pleyko.a@mail.ru>
2022-11-05 19:04:32 +0200

    пофиксила недочеты на 320

--- JS/index.js
+++ JS/index.js
@@ -1,40 +1,42 @@
-const doc = document;
-const log = console.log;
+
 let popUp = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-button')
 let editButton = document.querySelector('.profile__edit-button');
 let nameInfo = document.querySelector('.profile__title');
 let jobInfo = document.querySelector('.profile__subtitle');
 let formElement = document.querySelector('.popup__edit');
-let nameInput = formElement.querySelector('.popup__input_name');
-let jobInput = formElement.querySelector('.popup__input_info');
-let saveInfo = formElement.querySelector('.popup__save-button');
 
 
-doc.addEventListener("click", ({ target }) => {
+/*doc.addEventListener("click", ({ target }) => {
   const likeBtn = target.closest(".element__like")
 
   if (!likeBtn) return;
   likeBtn.ariaPressed = likeBtn.ariaPressed === "true" ? "false" : "true";
   likeBtn.ariaLabel = aria.label[likeBtn.ariaPressed];
 })
-  ;
+  ;*/
 
-//открытие попапа
+
 editButton.addEventListener('click', function () {
-  popUp.classList.add('opened');
+  popUp.classList.add('popup_opened');
 })
 
-closeButton.addEventListener('click', function () {
-  popUp.classList.remove('opened');
-})
+function closePopUp () {
+  popUp.classList.remove('popup_opened');
+};
 
-//попап закроется по клику по бэкграунду
+//форма в DOM
+let nameInput = document.querySelector('.popup__input-name');
+let jobInput = document.querySelector('.popup__input-info');
+
+//поля формы в DOM
+
+/*//попап закроется по клику по бэкграунду
 popUp.addEventListener('click', function (event) {
   if (event.target === event.currentTarget) {
     popUp.classList.remove('opened');
   }
-});
+});*/
 
 
 
@@ -45,8 +47,8 @@ function formSubmitHandler(evt) {
   jobInfo.textContent = jobInput.value;
 }
 
+closeButton.addEventListener('click', closePopUp);
+
 formElement.addEventListener('submit', formSubmitHandler);
 
-saveInfo.addEventListener('click', function () {
-  popUp.classList.remove('opened');
-})
+
