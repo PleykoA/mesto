f7bdf3557b56ecff1bd6b7c695fa5089dbe168af
Asya <pleyko.a@mail.ru>
2023-01-30 00:00:47 +0200

    удалила лишние переменные

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import './pages/index.css';
-import { initialCards, settings } from './components/constants.js';
-import { Card } from './components/Card.js';
-import { FormValidator } from './components/FormValidator.js';
-import { Section } from './components/Section.js';
-import { PopupWithForm } from './components/PopupWithForm.js';
-import { PopupWithImage } from './components/PopupWithImage.js';
-import { UserInfo } from './components/UserInfo.js';
+import './index.css';
+import { initialCards, settings } from '../utils/constants.js';
+import { Card } from '../components/Card.js';
+import { FormValidator } from '../components/FormValidator.js';
+import { Section } from '../components/Section.js';
+import { PopupWithForm } from '../components/PopupWithForm.js';
+import { PopupWithImage } from '../components/PopupWithImage.js';
+import { UserInfo } from '../components/UserInfo.js';
 
 //попапы
 const popupEditProfile = document.querySelector('.popup_edite');
